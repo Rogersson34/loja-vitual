19bc5ba487ae23825e0a0fc6cca9e4c4f91f1d17
Roger <rogersson2024@gmail.com>
2026-09-07 17:12:13 -0300

    [T002] - feat: Criando Array de produtos

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import './data/produtos.js'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,9 +17,9 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>Loja Virtual</h1>
           <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+            É aqui onde as coisas acontecem <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
         </div>
         <button
@@ -26,7 +27,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          Count is {count}
+          Account {count}
         </button>
       </section>
 
@@ -37,7 +38,7 @@ function App() {
           <svg className="icon" role="presentation" aria-hidden="true">
             <use href="/icons.svg#documentation-icon"></use>
           </svg>
-          <h2>Documentation</h2>
+          <h2>Card Imagens</h2>
           <p>Your questions, answered</p>
           <ul>
             <li>
@@ -58,7 +59,7 @@ function App() {
           <svg className="icon" role="presentation" aria-hidden="true">
             <use href="/icons.svg#social-icon"></use>
           </svg>
-          <h2>Connect with us</h2>
+          <h2>Card Cadastros</h2>
           <p>Join the Vite community</p>
           <ul>
             <li>
---
+++ src/data/produtos.js
@@ -0,0 +1,66 @@
+const produtos = [
+    {
+        id: 1,
+        nome: "Bicicleta",
+        precoUnitario: 900.00,
+        quantidade: 2
+    },
+    {
+        id: 2,
+        nome: "Celular",
+        precoUnitario: 1400.00,
+        quantidade: 5
+    },
+    {
+        id: 3,
+        nome: "Ferramentas",
+        precoUnitario: 149.90,
+        quantidade: 3
+    },
+    {
+        id: 4,
+        nome: "Impressora",
+        precoUnitario: 799.00,
+        quantidade: 2
+    },
+    {
+        id: 5,
+        nome: "Liquidificador",
+        precoUnitario: 89.90,
+        quantidade: 4
+    },
+    {
+        id: 6,
+        nome: "Notebook",
+        precoUnitario: 1999.00,
+        quantidade: 2
+    },
+    {
+        id: 7,
+        nome: "Panelas",
+        precoUnitario: 264.90,
+        quantidade: 5
+    },
+    {
+        id: 8,
+        nome: "Pneu",
+        precoUnitario: 399.00,
+        quantidade: 4
+    },
+    {
+        id: 9,
+        nome: "TV",
+        precoUnitario: 1699.00,
+        quantidade: 2
+    },
+    {
+        id: 10,
+        nome: "Video Game",
+        precoUnitario: 2999.00,
+        quantidade: 1
+    }
+
+];
+
+
+console.log(produtos);
