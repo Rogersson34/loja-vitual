c42c750c8015a3889e70890b8273ccc1400b83b1
Roger <rogersson2024@gmail.com>
2026-09-09 11:41:05 -0300

    [T005] - feat: Adicionando Carrinho.jsx

---
+++ src/pages/Carrinho.jsx
@@ -0,0 +1,14 @@
+function Carrinho() {
+
+    return (
+        <div className="pagina-carrinho">
+            <h1>🛍️Carrinho de Compras</h1>
+            <p>produtos, subtotais e total da compra</p>
+
+        </div>
+
+
+    )
+}
+
+export default Carrinho
