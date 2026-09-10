4b8d70ff2385315b6372f1c62ca7bea428d23661
Roger <rogersson2024@gmail.com>
2026-09-09 21:42:26 -0300

    [T008] - feat: Criando Pagamento.jsx

---
+++ src/pages/Pagamento.jsx
@@ -0,0 +1,94 @@
+import { useState } from 'react'
+
+function Pagamento({ total }) {
+
+    const [formaPagamento, setFormaPagamento] = useState('')
+
+    function finalizarPagamento(event) {
+
+        event.preventDefault()
+
+        if (formaPagamento === '') {
+            alert('Escolha uma forma de pagamento.')
+            return
+        }
+
+        alert(
+            `Pagamento de R$ ${total.toFixed(2)} realizado com ${formaPagamento}!`
+        )
+    }
+
+
+    return (
+        <div className="pagina-pagamento">
+
+            <h1>Pagamento</h1>
+
+            <p>
+                Total da compra:
+                <strong> R$ {total.toFixed(2)}</strong>
+            </p>
+
+
+            <form onSubmit={finalizarPagamento}>
+
+                <h2>Escolha a forma de pagamento</h2>
+
+
+                <label>
+                    <input
+                        type="radio"
+                        name="pagamento"
+                        value="Pix"
+                        checked={formaPagamento === 'Pix'}
+                        onChange={(event) =>
+                            setFormaPagamento(event.target.value)
+                        }
+                    />
+
+                    Pix
+                </label>
+
+
+                <label>
+                    <input
+                        type="radio"
+                        name="pagamento"
+                        value="Cartão de Crédito"
+                        checked={formaPagamento === 'Cartão de Crédito'}
+                        onChange={(event) =>
+                            setFormaPagamento(event.target.value)
+                        }
+                    />
+
+                    Cartão de Crédito
+                </label>
+
+
+                <label>
+                    <input
+                        type="radio"
+                        name="pagamento"
+                        value="Boleto"
+                        checked={formaPagamento === 'Boleto'}
+                        onChange={(event) =>
+                            setFormaPagamento(event.target.value)
+                        }
+                    />
+
+                    Boleto
+                </label>
+
+
+                <button type="submit">
+                    Finalizar Pagamento
+                </button>
+
+            </form>
+
+        </div>
+    )
+}
+
+export default Pagamento
+
